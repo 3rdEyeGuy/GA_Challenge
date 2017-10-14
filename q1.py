89eb9ffefc04dce117ba4e4195a2afeaa9e91704
Octavio <Oasosaf@gmail.com>
2017-10-14 18:47:21 -0500

    fix conversion attempt of '' to int

--- q1.py
+++ q1.py
@@ -1,13 +1,27 @@
+"""Sifts through Kick-starter project data and prints the total pledge average"""
+
 import csv
 
 #assign csv file to a readable object
 with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_file:
     csv_obj = csv.reader(csv_file)
-
+    
+    #skip variable title
     next(csv_obj)
 
-    #print pledge for each project
-    for line in csv_obj:
-        print(line[8])
+    #variable initialization
+    Totpledge = 0
+    count = 0
 
+    #count total pledge for each project and number of projects
+    for line in csv_obj:
+        if line[8] is not '':
+            Totpledge = int(line[8]) + Totpledge 
+            count = count + 1
+
+    #calculate average pledge amount
+    avgPledge = Totpledge/count
+    
+    #print the average pledge amount
+    print('The average amount pledged is $' + str(avgPledge))
      
