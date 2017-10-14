6365ea9f825752c671831ae6183c85b191c19e7a
Octavio <Oasosaf@gmail.com>
2017-10-14 11:53:42 -0500

    print pledge of projects & skip title

--- q1.py
+++ q1.py
@@ -1,9 +1,13 @@
 import csv
 
+#assign csv file to a readable object
 with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_file:
     csv_obj = csv.reader(csv_file)
 
-    for line in csv_obj:
-        print(line[1])
+    next(csv_obj)
 
-    
+    #print pledge for each project
+    for line in csv_obj:
+        print(line[8])
+
+     
