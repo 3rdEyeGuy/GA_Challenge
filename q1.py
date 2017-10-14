c0b5414f8daf3ca5533d3bdf3d268630bafe291e
Octavio <Oasosaf@gmail.com>
2017-10-14 11:47:25 -0500

    encode unknown character byte 0x92 into readable

--- q1.py
+++ q1.py
@@ -1,9 +1,9 @@
 import csv
 
-with open('DSI_kickstarterscrape_dataset.csv','r') as csv_file:
+with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_file:
     csv_obj = csv.reader(csv_file)
 
     for line in csv_obj:
         print(line[1])
 
-
+    
