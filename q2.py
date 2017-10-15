65486e57d95cdf0575d182d0ccfaeb97d29705b9
Octavio <Oasosaf@gmail.com>
2017-10-15 10:30:31 -0500

    include comments for all lines

--- q2.py
+++ q2.py
@@ -1,11 +1,12 @@
 """Creates a list of number of backers within bins of 100"""
+#imports csv class
 import csv
 
 #assign csv file to a readable object
 with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_file:
     csv_obj = csv.reader(csv_file)
     
-    #skip variable title
+    #skip headers 
     next(csv_obj)
 
     #variable initialization
@@ -16,22 +17,32 @@ with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_
         if line[10] is not '':
             backers.append(int(line[10]))
 
+#init variables
 frq = 0
 bin_ct = 100
 
+#init bin range variables
 a = 0
 b = bin_ct
 
+#Until the bin range encompasses the largest number of backers
+#sift through the data for each bin range and append the "bin,frequency" to
+#a csv file
 while (b-bin_ct) <= max(backers):
+    #init bin range
     rng = list(range(a,b+1))
 
+    #sift through every point within each bin and update the frequency
+    #for the number of backers within each bin
     for line in backers:
         if rng[0] <= line <= rng[-1]:
             frq = frq + 1
 
+    #append "bin,frequency" to a csv file
     print(str(b) + ':',str(frq))
     frq = 0
     
+    #update the bin range for next frequency count
     if a == 0:
         a = a + bin_ct + 1
         b = b + bin_ct
