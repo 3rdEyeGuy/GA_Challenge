6b7cd29776b81d6dc268f934f8d9502588837bfb
Octavio <Oasosaf@gmail.com>
2017-10-15 14:39:21 -0500

    create list of backers and display histogram

--- q2.py
+++ q2.py
@@ -1,6 +1,6 @@
-"""Creates a list of number of backers within bins of 100"""
-#imports csv class
+"""Creates a list of backers per project and displays a histogram of that data"""
 import csv
+import matplotlib.pyplot as plt
 
 #assign csv file to a readable object
 with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_file:
@@ -20,4 +20,16 @@ with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_
     #close file object
     csv_file.close() 
 
+#init bin size
+incrmt = 100
+#init list of bins
+bins = list(range(0, max(backers)+incrmt, incrmt))
 
+#create histogram
+plt.hist(backers,bins, histtype='bar',rwidth=1.0)
+plt.ylabel('Frequency')
+plt.xlabel('Backers')
+plt.title('Histogram of Backers')
+
+#show histogram
+plt.show()
