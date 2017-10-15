276d0153f0cef6fd9ff5a3547b028b13b684d661
Octavio <Oasosaf@gmail.com>
2017-10-15 10:12:46 -0500

    print "bin:frequency" for a bin of 100

--- q2.py
+++ q2.py
@@ -1,4 +1,4 @@
-"""Creates a list of number of backers within bins of 10"""
+"""Creates a list of number of backers within bins of 100"""
 import csv
 
 #assign csv file to a readable object
@@ -10,24 +10,17 @@ with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_
 
     #variable initialization
     backers = [] 
-    num_camps = 0
 
     #count total pledge for each project and number of projects
     for line in csv_obj:
         if line[10] is not '':
             backers.append(int(line[10]))
-            num_camps = num_camps + 1
-
-print('Max backers for single campaign:', max(backers))
 
 frq = 0
-last_camp = 0
-
-bin_ct = 10 
+bin_ct = 100
 
 a = 0
 b = bin_ct
-
 
 while (b-bin_ct) <= max(backers):
     rng = list(range(a,b+1))
@@ -45,6 +38,3 @@ while (b-bin_ct) <= max(backers):
     elif a != 0:
         a = a + bin_ct
         b = b + bin_ct
-    
-    last_camp = last_camp +1 
-
