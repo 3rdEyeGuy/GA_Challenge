1a564d073ea7eee207b0309ccd7e08c9c77a95dc
Octavio <Oasosaf@gmail.com>
2017-10-15 14:16:11 -0500

    append number of backers for each project to list

--- q2.py
+++ q2.py
@@ -4,22 +4,20 @@ import csv
 
 #assign csv file to a readable object
 with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_file:
-    csv_obj = csv.reader(csv_file)
+    csv_reader = csv.reader(csv_file)
     
     #skip headers 
-    next(csv_obj)
+    next(csv_reader)
+    
+    #init list
+    backers = []
 
-    #opens csv file to write 
-    with open('backers_hist_data.csv', 'w') as hist_obj:
-        hist_data = csv.writer(hist_obj)
-
-        #count total pledge for each project and number of projects
-        for line in csv_obj:
-            if line[10] is not '':
-                hist_data.writerow(int(line[10]))
-
-        #close file object
-        hist_obj.close()    
-
+    #append backers for each project to a list
+    for line in csv_reader:
+        if line[10] is not '':
+            backers.append(int(line[10]))
+    
     #close file object
     csv_file.close() 
+
+
