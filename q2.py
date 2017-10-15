7e24bcb327f0ebb103271b7e79e71b232d97dea4
Octavio <Oasosaf@gmail.com>
2017-10-15 16:29:58 -0500

    insert skew within histogram plot

--- q2.py
+++ q2.py
@@ -1,6 +1,7 @@
 """Creates a list of backers per project and displays a histogram of that data"""
 import csv
 import matplotlib.pyplot as plt
+from scipy.stats import skew
 
 #assign csv file to a readable object
 with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_file:
@@ -20,6 +21,9 @@ with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_
     #close file object
     csv_file.close() 
 
+#calculates Pearson's skewness coefficient
+skew = skew(backers)
+
 #init bin size
 incrmt = 100
 #init list of bins
@@ -30,6 +34,10 @@ plt.hist(backers,bins, histtype='bar',rwidth=1.0)
 plt.ylabel('Frequency')
 plt.xlabel('Backers')
 plt.title('Kickstarter Campaign Backers')
+
+#inserts skewness 1/2 way across x-axis and 3/4 up the y-axis
+#transform allows me to create axes at which 1,1 marks the top right corner of plot
+plt.text(0.5,0.75,r'Skew ={}'.format(skew),transform=plt.gca().transAxes) 
 
 #show histogram
 plt.show()
