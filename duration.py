a5741b4044c1659891814d5f086d67441eb69964
Octavio <Oasosaf@gmail.com>
2017-10-16 11:55:33 -0500

    display histogram of the durations of successful campaigns

--- duration.py
+++ duration.py
@@ -1,4 +1,4 @@
-"""Plots bar graph of duration vs. funded percent"""
+"""Plots histogram of durations for successful campaigns"""
 
 #import classes and modules
 import csv
@@ -14,33 +14,32 @@ with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_
     
     #init list
     duration = []
-    fundPrcent = []
     
     #append duration for each project into a list
     for line in csv_reader:
-        fundPrcent.append(float(line[9]))
-        duration.append(float(line[16]))
+        if line[6] == 'successful':
+            duration.append(float(line[16]))
 
     #close file object
     csv_file.close() 
 
 #calculates Pearson's skewness coefficient
-###skew = skew(duration)
+skew = skew(duration)
 
 #init bin size
-###incrmt = 100
+incrmt = 5 
 #init list of bins
-###bins = list(range(0, max(duration)+incrmt, incrmt))
+bins = list(range(0, int(max(duration)+incrmt), incrmt))
 
 #create histogram
-plt.bar(duration,fundPrcent)
-plt.ylabel('Funded Percentages')
-plt.xlabel('Campaign Duration')
-plt.title('Kickstarter Campaign Durations vs. Funded Percentages')
+plt.hist(duration,bins, histtype='bar',rwidth=1.0)
+plt.ylabel('Number of Successful Campaigns')
+plt.xlabel('Campaign Duration (days)')
+plt.title('Durations of Successful Kickstarter Campaigns')
 
 #inserts skewness 1/2 way across x-axis and 3/4 up the y-axis
 #transform allows me to create axes at which 1,1 marks the top right corner of plot
-###plt.text(0.5,0.75,r'Skew ={}'.format(skew),transform=plt.gca().transAxes) 
+plt.text(0.5,0.75,r'Skew ={}'.format(skew),transform=plt.gca().transAxes) 
 
 #show histogram
 plt.show()
