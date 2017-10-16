f3e9674830d07efd8246ef0ff2a59fa046aed959
Octavio <Oasosaf@gmail.com>
2017-10-16 12:26:36 -0500

    display scatterplot of durations vs. pledge amounts of successful campaigns

--- duration.py
+++ duration.py
@@ -14,32 +14,34 @@ with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_
     
     #init list
     duration = []
-    
+    pledge = [] 
+
     #append duration for each project into a list
     for line in csv_reader:
-        if line[6] == 'failed':
+        if line[6] == 'successful':
             duration.append(float(line[16]))
+            pledge.append(int(line[8]))
 
     #close file object
     csv_file.close() 
 
 #calculates Pearson's skewness coefficient
-skew = skew(duration)
+###skew = skew(duration)
 
 #init bin size
-incrmt = 5 
+###incrmt = 5 
 #init list of bins
-bins = list(range(0, int(max(duration)+incrmt), incrmt))
+###bins = list(range(0, int(max(duration)+incrmt), incrmt))
 
 #create histogram
-plt.hist(duration,bins, histtype='bar',rwidth=1.0)
-plt.ylabel('Number of Failed Campaigns')
+plt.scatter(duration,pledge)
+plt.ylabel('Pledge Amount ($)')
 plt.xlabel('Campaign Duration (days)')
-plt.title('Durations of Failed Kickstarter Campaigns')
+plt.title('Durations vs. Pledge Amounts of Successful Kickstarter Campaigns')
 
 #inserts skewness 1/2 way across x-axis and 3/4 up the y-axis
 #transform allows me to create axes at which 1,1 marks the top right corner of plot
-plt.text(0.5,0.75,r'Skew ={}'.format(skew),transform=plt.gca().transAxes) 
+###plt.text(0.5,0.75,r'Skew ={}'.format(skew),transform=plt.gca().transAxes) 
 
 #show histogram
 plt.show()
