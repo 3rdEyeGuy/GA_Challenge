eb1301058ba577659b2d31380c65787f33f870e3
Octavio <Oasosaf@gmail.com>
2017-10-16 21:02:05 -0500

    remove duplicates from original data set

--- q1.py
+++ q1.py
@@ -4,22 +4,38 @@ import statistics
 
 #assign csv file to a readable object
 with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_file:
-    csv_obj = csv.reader(csv_file)
+    csv_reader = csv.reader(csv_file)
     
-    #skip variable title
-    next(csv_obj)
+    #skip headers 
+    next(csv_reader)
 
     #init list of pledges
     pledge=[]
 
+    #init list and dict
+    dataRaw = []
+    data = []
+
+
+    #append entire data set into a list
+    for line in csv_reader:
+        dataRaw.append([int(line[0]),str(line[8])])
+
+    #close file object
+    csv_file.close()
+
+    #append only unique data into a list
+    for dataR in dataRaw:
+        if dataR[0] in data:
+            continue
+        else: data.append([dataR[0],dataR[1]])
+
     #create list for total pledge of each project
-    for line in csv_obj:
-        if line[8] is not '':
-            pledge.append(int(line[8]))
+    for line in data:
+        if line[1] is not '':
+            pledge.append(int(line[1]))
 
     mean = statistics.mean(pledge)
     #print the average pledge amount
     print('The mean amount pledged was $' + str(mean))
 
-    #close file object
-    csv_file.close()
