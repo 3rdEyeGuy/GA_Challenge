722353dfae5c807257069cc0bebfd816ddb33ba9
Octavio <Oasosaf@gmail.com>
2017-10-16 19:58:49 -0500

    remove data duplicates and print bar graph of category vs fund percent

--- type.py
+++ type.py
@@ -5,6 +5,7 @@ import csv
 import matplotlib.pyplot as plt
 from scipy.stats import skew
 import statistics
+import collections
 
 #assign csv file to a readable object
 with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_file:
@@ -13,44 +14,53 @@ with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_
     #skip headers 
     next(csv_reader)
     
-    #init list
+    #init list and dict
+    dataRaw = []
+    data = []
+    typeDict = {}
     types = []
 
-    typeDict = {}
-
-    #append types for each project into a list
+    #append entire data set into a list
     for line in csv_reader:
         if line[6] == 'successful':
-            if line[3] in types:
-                continue
-            else: types.append(str(line[3]))
-
-    csv_file.close()
-#assign csv file to a readable object
-with open('DSI_kickstarterscrape_dataset.csv','r', encoding='mac_roman') as csv_file:
-    csv_reader = csv.reader(csv_file)
-
-    for typ in types:
-        print('typ loop:', typ)
-        fundPrcnt = []
-        for line in csv_reader:
-            if line[6] == 'successful':
-                if typ == str(line[3]):
-                    fundPrcnt.append(float(line[9]))
-        avgfundPrcnt = statistics.mean(fundPrcnt)
-        typeDict[typ] = avgfundPrcnt 
-        print(typeDict) 
+            dataRaw.append([int(line[0]),str(line[3]),float(line[9])])
 
     #close file object
-    csv_file.close() 
+    csv_file.close()
 
-#create histogram
+#Append only unique data into a list
+for dataR in dataRaw:
+    if dataR[0] in data:
+        continue
+    else: data.append([dataR[1],dataR[2]])
+
+#creates a list of unique types
+for typF in data:
+    if typF[0] in types:
+        continue
+    else: types.append(typF[0])
+
+#create a dict with campaign categories and funding percentages
+for typ in types:
+    fundPrcnt = []
+    for fund in data:
+        if typ == fund[0]:
+            fundPrcnt.append(float(fund[1]))
+    avgfundPrcnt = statistics.mean(fundPrcnt)
+    typeDict[typ] = avgfundPrcnt 
+
+#sort dict
+typeDict = collections.OrderedDict(sorted(typeDict.items()))
+print(typeDict) 
+
+#create bar graph from dict
 plt.bar(range(len(typeDict)), typeDict.values(), align = 'center')
 plt.xticks(range(len(typeDict)), typeDict.keys())
 
+#set bar graph title and labels
 plt.ylabel('Average Funded Percent') 
 plt.xlabel('Categories')
 plt.title('The Average Funded Percents of Kickstarter Campaign Categories')
 
-#show histogram
+#show bar graph
 plt.show()
